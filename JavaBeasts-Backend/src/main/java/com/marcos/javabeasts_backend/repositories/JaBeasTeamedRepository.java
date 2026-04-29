3082e62993a454a1edbc19e37110381fe7d2230a
Marcos <marcosmiquel77@gmail.com>
2026-04-29 15:44:28 +0200

    repo de JaBeasTeamed con consultas

--- JavaBeasts-Backend/src/main/java/com/marcos/javabeasts_backend/repositories/JaBeasTeamedRepository.java
+++ JavaBeasts-Backend/src/main/java/com/marcos/javabeasts_backend/repositories/JaBeasTeamedRepository.java
@@ -4,9 +4,12 @@ import com.marcos.javabeasts_backend.entity.JaBeasTeamed;
 import com.marcos.javabeasts_backend.entity.embeddables.JaBeaTeamedId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JaBeasTeamedRepository extends JpaRepository<JaBeasTeamed, JaBeaTeamedId> {
-    JaBeasTeamed findByIdTeamIdAndIdSlot(Integer teamId, Integer slot);
+    List<JaBeasTeamed> findByTeamTeamIdOrderByIdSlotAsc(Integer teamId);
+
+    Optional<JaBeasTeamed> findByTeamTeamIdAndIdSlot(Integer teamId, Integer slot);
 
 }
