ee2f397f9e369cc00c03d791256c69094ad985c3
Marcos <marcosmiquel77@gmail.com>
2026-05-13 16:53:08 +0200

    implementadas las imagenes nuevas

--- JavaBeasts-Mobile/constants/jabea-images.ts
+++ JavaBeasts-Mobile/constants/jabea-images.ts
@@ -2,13 +2,24 @@ import { ImageSourcePropType } from 'react-native';
 
 const JABEA_IMAGES: Record<string, ImageSourcePropType> = {
   aquaryx: require('../assets/jabeas/aquaryx.png'),
+  branther: require('../assets/jabeas/branther.png'),
+  brawlex: require('../assets/jabeas/brawlex.png'),
   cindrel: require('../assets/jabeas/cindrel.png'),
+  codrex: require('../assets/jabeas/codrex.png'),
+  electryn: require('../assets/jabeas/electryn.png'),
   flamara: require('../assets/jabeas/flamara.png'),
+  florion: require('../assets/jabeas/florion.png'),
+  griffel: require('../assets/jabeas/griffel.png'),
   ignarok: require('../assets/jabeas/ignarok.png'),
   marvyn: require('../assets/jabeas/marvyn.png'),
   nerulon: require('../assets/jabeas/nerulon.png'),
   pyronox: require('../assets/jabeas/pyronox.png'),
+  stormix: require('../assets/jabeas/stormix.png'),
+  swiftor: require('../assets/jabeas/swiftor.png'),
+  sylphyra: require('../assets/jabeas/sylphyra.png'),
   tiderra: require('../assets/jabeas/tiderra.png'),
+  voltari: require('../assets/jabeas/voltari.png'),
+  zapphir: require('../assets/jabeas/zapphir.png'),
 };
 
 const PLACEHOLDER_IMAGE = require('../assets/jabeas/placeholder.png');
